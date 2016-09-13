f22f76a62dbb7038b1af2114d73937eb4511aadc
Brendan <bkrone1@gmail.com>
2016-09-13 16:34:35 -0400

    completed challenge 2

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,25 @@
 $(function(){
 
-	//all your code should go here
+	 var replaced = $("body").html().replace(/Bacon/g, "Laser Vision");
+    $("body").html(replaced);
 
-})
+    $('.title:even').hide();
+    $('footer li:even').hide();
+
+  	$('.post:last').remove();
+	$('.post:last').remove();
+	$('.post:last').remove();
+
+	$('aside').remove();
+
+    var state = true;
+    $( "h1" ).on("click", function() {
+      if ( state ) {       
+         $("h1").css({color: "#ff0000"});
+       } else {
+         $("h1").css({color: "#000000"});
+      }
+      state = !state;
+    });
+
+  } );
